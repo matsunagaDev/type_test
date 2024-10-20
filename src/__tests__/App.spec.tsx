7582f61e9ecb2ff6b8f6da0765ec52a111fc9fa6
masarumatsunaga <65140122+masarumatsunaga@users.noreply.github.com>
2024-10-20 22:01:51 +0900

    test fixed

--- src/__tests__/App.spec.tsx
+++ src/__tests__/App.spec.tsx
@@ -1,25 +1,14 @@
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen, waitFor } from "@testing-library/react";
+import App from "../App";
 import '@testing-library/jest-dom';
-import App from '../App';
 
-// createClient関数をモック
-jest.mock('@supabase/supabase-js', () => ({
-  createClient: jest.fn(() => ({
-    from: jest.fn().mockReturnThis(),
-    select: jest.fn().mockResolvedValue({ data: [], error: null }),
-    insert: jest.fn().mockResolvedValue({ error: null }),
-    delete: jest.fn().mockResolvedValue({ data: [], error: null }),
-    eq: jest.fn().mockReturnThis(),
-  })),
-}));
 
-describe('App', () => {
-  test('タイトルがあること', async () => {
+describe("App", () => {
+  test("タイトルがあること", async () => {
     render(<App />);
-    const title = screen.getByTestId('title');
-
     await waitFor(() => {
+      const title = screen.getByTestId("title");
       expect(title).toBeInTheDocument();
-    });
+    })
   });
 });
