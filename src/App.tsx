fef25d71f74c6c8ce4ff54ece7a1fbc20d7fcc36
masarumatsunaga <65140122+masarumatsunaga@users.noreply.github.com>
2024-10-21 22:09:07 +0900

    mock作成

--- src/App.tsx
+++ src/App.tsx
@@ -14,19 +14,29 @@ import {
 
 function App() {
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     const getAllTodos = async () => {
       const todosData = await GetAllTodos();
+      // モックした値が返ってくる
+      console.log(todosData);
+
       setTodos(todosData);
+      setIsLoading(false);
     };
     getAllTodos();
   }, []);
+
+  if (isLoading) {
+    return <p>Loading.....</p>;
+  }
 
   return (
     <>
       <h1 data-testid="title">TODOリスト</h1>
       <TableContainer width={{ base: 'full', md: '25%' }} mx="auto">
-        <Table variant="bubble" size="sm">
+        <Table variant="simple" data-testid="table">
           <TableCaption placement="top">Spending By Month</TableCaption>
           <Thead>
             <Tr>
